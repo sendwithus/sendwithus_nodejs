4bec5dbd3b2fad17aac9628de9977be2436d3504
Matt Harris <harrismc@gmail.com>
2013-01-09 17:32:51 -0800

    node js library ready to go

--- sendwithus.js
+++ sendwithus.js
@@ -1,19 +1,20 @@
+var API = require('./lib/api');
 
 var Sendwithus = function(key, options) {
-    this.API_KEY = key;
-
-    this.API_HOST = "API_HOST" in options ? options['API_HOST'] : "api.sendwithus.com";
-    this.API_PORT = "API_PORT" in options ? options['API_PORT'] : "443";
-    this.API_PROTO = "API_PROTO" in options ? options['API_PROTO'] : "https";
-    this.API_VERSION = "API_VERSION" in options ? options['API_VERSION'] : "0";
-    this.API_HEADER_KEY = "X-SWU-API-KEY";
-    this.DEBUG = "DEBUG" in options ? options['DEBUG'] : false;
+    this.api = new API(key, options);
 };
 
 Sendwithus.prototype = {
     send: function(email_name, email_to, context) {
+        var endpoint = 'send';
+
+        context.email_name = email_name;
+        context.email_to = email_to;
+
+        return this.api.post(endpoint, context);
     
     };
 };
 
 module.exports = Sendwithus;
+
