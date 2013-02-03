ffb1fa8fe3cdb4fb887333e410ae544804a2c184
Matt Harris <harrismc@gmail.com>
2013-02-02 16:52:44 -0800

    updated to support https endpoint

--- test/run.js
+++ test/run.js
@@ -4,14 +4,12 @@ var API = require("../sendwithus");
 var key = 'THIS_IS_A_TEST_API_KEY';
 
 var options = {
-    'API_HOST': 'beta.sendwithus.com',
-    'API_PORT': 80,
-    'API_PROTO': 'http',
     'DEBUG': true
 };
 
 var api = new API(key, options);
 
 console.log('> testing send ...');
+
 api.send('test', 'test@sendwithus.com', {'name': 'Jimmy the snake'});
 
