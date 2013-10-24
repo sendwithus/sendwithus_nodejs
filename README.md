98e36c3a48ed0a6efb9cd10d1eec625e7e20e45a
Gregory Schier <gschier1990@gmail.com>
2013-10-23 20:00:09 -0700

    Complete rewrite

--- README.md
+++ README.md
@@ -1,4 +1,113 @@
-sendwithus_node
-===============
+sendwithus node-client
+========================
 
-Send With Us node client
+## Installation
+
+```
+npm install sendwithus
+```
+
+# Usage
+
+All callbacks are accept `err` and `data`:
+
+```javascript
+var callback = function(err, data) {
+    if (err) {
+        console.log(err, err.statusCode);
+    } else {
+        console.log(data);
+    }
+};
+```
+
+## List Your Emails
+
+```javascript
+var api = require('sendwithus')(API_KEY);
+api.emails(callback);
+```
+
+## Send an Email
+
+### Call with REQUIRED parameters only
+
+The `email_data` field is optional, but highly recommended!
+
+```javascript
+var api = require('sendwithus')(API_KEY);
+api.send({
+    email_id: EMAIL_ID,
+    recipient: { address: 'us@sendwithus.com'}
+}, callback);
+```
+
+### Call with REQUIRED parameters and email_data
+```javascript
+var api = require('sendwithus')(API_KEY);
+api.send({
+    email_id: EMAIL_ID,
+    recipient: {
+        address: 'us@sendwithus.com', // required
+        name: 'Matt and Brad' 
+    },
+    email_data: { first_name: 'Matt' } 
+}, callback);
+```
+
+### Optional Sender
+`sender['address']` is a required sender field
+
+```javascript
+var api = require('sendwithus')(API_KEY);
+api.send({
+    email_id: EMAIL_ID,
+    recipient: { address: 'us@sendwithus.com'},
+    email_data: { first_name: 'Matt' },
+    sender: {
+        address: 'company@company.com', // required
+        name: 'Company' 
+    }
+}, callback);
+```
+
+### Optional Sender with reply_to address
+`sender['name']` and `sender['reply_to']` are both optional
+
+```javascript
+var api = require('sendwithus')(API_KEY);
+api.send({
+    email_id: EMAIL_ID,
+    recipient: { address: 'us@sendwithus.com'},
+    email_data: { first_name: 'Matt' },
+    sender: {
+        address: 'company@company.com', // required
+        name: 'Company',
+        reply_to: 'info@company.com'
+    }
+}, callback);
+```
+
+## expected response
+
+### Error cases
+
+#### malformed request
+	
+```javascript
+	> err.statusCode;
+	400
+```
+
+#### bad api key
+
+```javascript
+	> err.statusCode;    
+	403
+```
+
+## Run Tests
+
+```
+npm test
+```
---
+++ test/sendwithus.js
@@ -0,0 +1,111 @@
+var API_KEY         = 'THIS_IS_A_TEST_API_KEY';
+var INVALID_API_KEY = 'This_IS_A_BAD_API_KEY';
+var EMAIL_ID        = 'test_fixture_1';
+
+var sendwithusFactory = require('../lib/sendwithus');
+
+module.exports.send = {
+	setUp: function(callback) {
+		this.sendwithus    = sendwithusFactory(API_KEY);
+		this.sendwithusBad = sendwithusFactory(INVALID_API_KEY);
+
+		this.recipient = {
+			name: 'Company',
+			address: 'company@company.com'
+		};
+
+		this.imcompleteRecipient = {
+			name: 'Company'
+		};
+
+		this.sender = {
+			name: 'Sender',
+			address: 'sender@sender.com'
+		};
+
+		callback();
+	},
+	tearDown: function(callback) {
+		callback();
+	},
+	noData: function(test) {
+		var that = this;
+		var data = {
+			email_id: EMAIL_ID,
+			recipient: this.recipient
+		};
+
+		this.sendwithus.send(data, function(err, data) {
+			test.ifError(err);
+			test.ok(data.success, 'response was not successful');
+			test.done();
+		});
+	},
+	withData: function(test) {
+		var that = this;
+		var data = {
+			email_id: EMAIL_ID,
+			recipient: this.recipient,
+			email_data: { hello: 'World!' }
+		};
+
+		this.sendwithus.send(data, function(err, data) {
+			test.ifError(err);
+			test.ok(data.success, 'response was not successful');
+			test.done();
+		});
+	},
+	incompleteRecipient: function(test) {
+		var that = this;
+		var data = {
+			email_id: EMAIL_ID,
+			recipient: this.imcompleteRecipient
+		};
+
+		this.sendwithus.send(data, function(err, data) {
+			test.ok(err, 'no error was thrown');
+			test.equals(err.statusCode, 400, 'Wrong status code');
+			test.notStrictEqual(data.success, true, 'response was successful');
+			test.done();
+		});
+	},
+	invalidAPIKey: function(test) {
+		var that = this;
+		var data = {
+			email_id: EMAIL_ID,
+			recipient: this.imcompleteRecipient
+		};
+
+		this.sendwithusBad.send(data, function(err, data) {
+			test.ok(err, 'no error was thrown');
+			test.equals(err.statusCode, 403, 'Wrong status code');
+			test.notStrictEqual(data.success, true, 'response was successful');
+			test.done();
+		});
+	}
+};
+
+module.exports.emails = {
+	setUp: function(callback) {
+		this.sendwithus    = sendwithusFactory(API_KEY);
+		this.sendwithusBad = sendwithusFactory(INVALID_API_KEY);
+
+		callback();
+	},
+	tearDown: function(callback) {
+		callback();
+	},
+	list: function(test) {
+		this.sendwithus.emails(function(err, data) {
+			test.ifError(err);
+			test.done();
+		});
+	},
+	listInvalidAPIKey: function(test) {
+		this.sendwithusBad.emails(function(err, data) {
+			test.ok(err);
+			test.equals(err.statusCode, 403, 'Wrong status code');
+			test.done();
+		});
+	}
+};
